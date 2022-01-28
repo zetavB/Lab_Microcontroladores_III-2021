b497e004b17910c8e6c8a61aa2f6ed5d0042e6e7
ecerdasr <edward.cerdas@ucr.ac.cr>
2022-01-28 14:29:46 -0600

    agregado el termistor al circuito y algunas pruebas en el código para entender el funcionamiento

--- Lab3/incubadora.ino
+++ Lab3/incubadora.ino
@@ -1,18 +1,45 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-//https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+int vtermistor;
+float vtermistorNorm;
+float R1 = 100000; //resistencia de 100k en serie con el termistor.
+float logR2, R2, TEMPERATURA;
+float c1 = 2.115e-03, c2 = 0.383e-04, c3 = 5.228e-07; //coeficientes de steinhart-hart, obtenidos de: https://www.thinksrs.com/downloads/programs/Therm%20Calc/NTCCalibrator/NTCcalculator.htm
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+
+void blink(){
+  //led parpadea una vez cada vez que inicia el loop
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT); //led integrado como salida
+}
+
+// loop infinito
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
+  blink();
+
+  vtermistor = analogRead(A0);
+  vtermistorNorm = vtermistor/204.6; //convertimos el valor de 8 bits (0-1023) a un valor entre 0 y 5
+  R2 = R1 * (1023.0 / (float)vtermistor - 1.0);
+  logR2 = log(R2);
+  TEMPERATURA = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); //ecuacion de S-H, da temperatura en kelvin
+  TEMPERATURA = TEMPERATURA -273.15; //convertimos a grados centigrados la temperatura
+
+  Serial.print("Temperatura: ");
+  Serial.print(TEMPERATURA);
+
+  if ((vtermistorNorm > 3)){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else{
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
